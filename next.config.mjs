bb40ef9bf2713abc8402dd1e10bcff7f3d118301
mihiragath <mihiragath@users.noreply.github.com>
2025-07-22 21:53:28 +0530

    add

--- next.config.mjs
+++ next.config.mjs
@@ -8,15 +8,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "vnxpfgobegkxdtgsfsag.supabase.co",
-      },
-      {
-        protocol: "https",
-        hostname: "img.clerk.com",
-      },
-      {
-        protocol: "https",
-        hostname: "images.clerk.dev",
+        hostname: "vnxpfgobegkxdtgsfsag.supabase.co", 
       },
     ],
   },
@@ -24,29 +16,11 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-      {
         source: "/embed",
         headers: [
           {
             key: "Content-Security-Policy",
-            value: "frame-src 'self' https://roadsidecoder.created.app;",
+            value: "https://mihiragath.created.app;",
           },
         ],
       },
